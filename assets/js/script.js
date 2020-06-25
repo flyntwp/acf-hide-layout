cce554a87455328f888c65f81ef6cff0661713a8
Harun Bašić <harun@bleech.de>
2020-06-25 10:59:32 +0200

    fix: check if window.acf exists

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,43 @@
 (function( $ ) {
     'use strict';
 
-    window.acf.addAction( 'ready_field/type=flexible_content', function( field ) {
-        var hidden_layouts = window.acf_hide_layout_options.hidden_layouts[field.data.key];
+    if ( window.acf ) {
+        window.acf.addAction( 'ready_field/type=flexible_content', function( field ) {
+            var hidden_layouts = window.acf_hide_layout_options.hidden_layouts[field.data.key];
 
-        // for each layout in the flexible field
-        field.$el.find( '.layout' ).each(function( i, element ) {
-            var $el = $( element ),
-                $controls = $el.find( '.acf-fc-layout-controls' ),
-                index = $el.attr( 'data-id' ),
-                name = 'acf[' + field.data.key + '][' + index + '][acf_hide_layout]',
-                in_array = -1 !== $.inArray( index, hidden_layouts ),
-                is_hidden = in_array && 'acfcloneindex' !== index;
+            // for each layout in the flexible field
+            field.$el.find( '.layout' ).each(function( i, element ) {
+                var $el = $( element ),
+                    $controls = $el.find( '.acf-fc-layout-controls' ),
+                    index = $el.attr( 'data-id' ),
+                    name = 'acf[' + field.data.key + '][' + index + '][acf_hide_layout]',
+                    in_array = -1 !== $.inArray( index, hidden_layouts ),
+                    is_hidden = in_array && 'acfcloneindex' !== index;
 
-            var $input = $( '<input>', {
-                type: 'hidden',
-                name: name,
-                class: 'acf-hide-layout',
-                value: is_hidden ? '1' : '0',
+                var $input = $( '<input>', {
+                    type: 'hidden',
+                    name: name,
+                    class: 'acf-hide-layout',
+                    value: is_hidden ? '1' : '0',
+                });
+
+                var $action = $( '<a>', {
+                    'data-index': index,
+                    'data-name': 'hide-layout',
+                    href: '#',
+                    title: window.acf_hide_layout_options.i18n.hide_layout,
+                    class: 'acf-icon dashicons acf-hide-layout small light acf-js-tooltip',
+                });
+
+                $action.prepend( $input );
+                $controls.prepend( $action );
+
+                if ( is_hidden ) {
+                    $el.addClass( 'acf-layout-hidden' );
+                }
             });
-
-            var $action = $( '<a>', {
-                'data-index': index,
-                'data-name': 'hide-layout',
-                href: '#',
-                title: window.acf_hide_layout_options.i18n.hide_layout,
-                class: 'acf-icon dashicons acf-hide-layout small light acf-js-tooltip',
-            });
-
-            $action.prepend( $input );
-            $controls.prepend( $action );
-
-            if ( is_hidden ) {
-                $el.addClass( 'acf-layout-hidden' );
-            }
         });
-    });
+    }
 
     $( document ).on( 'click', '.acf-hide-layout', function() {
         var $el = $( this ),
